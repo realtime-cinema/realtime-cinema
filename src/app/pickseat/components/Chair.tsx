29ed3e592d72368559af5203db21d1ede87fa1f5
Đậu Khoa <khoakomlem@gmail.com>
2024-04-03 23:39:01 +0700

    new feauter!

--- src/app/pickseat/components/Chair.tsx
+++ src/app/pickseat/components/Chair.tsx
@@ -4,6 +4,8 @@ import { cn } from '@/utils/common';
 
 export function Chair({ color }: { readonly color: string }) {
   return (
-    <div className={cn('Table', `Table_${color}`, 'triet ngu vl')}>Axx</div>
+    <div className={cn('Table', `Table_${color}`, 'triet ngu vlasdasdasdasd')}>
+      Axx
+    </div>
   );
 }
