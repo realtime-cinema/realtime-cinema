68e6637051a655d459f73187d728136ea60a6194
Sang Virgo <tansang06092004@gmail.com>
2024-04-07 17:13:32 +0700

    hih

--- src/app/film/components/filmDangChieu.tsx
+++ src/app/film/components/filmDangChieu.tsx
@@ -6,7 +6,9 @@ import PosterFilm from './PosterFilm';
 export default function FilmDangChieu() {
   return (
     <div className=''>
-      <section className="scroll-margin-top bg-[url('https://homepage.momocdn.net/img/momo-upload-api-210701105436-637607336767432408.jpg')] bg-black bg-contain bg-bottom bg-no-repeat py-8 md:py-10 lg:py-14 relative ">
+      <section className="scroll-margin-top bg-[url('https://homepage.momocdn.net/img/momo-upload-api-210701105436-637607336767432408.jpg')] bg-black bg-cover bg-no-repeat py-8 md:py-10 lg:py-14 relative bg-[size-y:50%] width"
+      style={{ height: '50%', width: '100%' }}
+      >
         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full text-white text-center max-w-6xl px-5 md:px-8 lg:px-8'>
           <div className='mb-5 text-center md:mb-5 mt-[-20px] font-bold'>
             Phim đang chiếu
@@ -17,11 +19,11 @@ export default function FilmDangChieu() {
               <div className='swiper-cinema relative'>
                 <div className='swiper swiper-initialized swiper-horizontal swiper-pointer-events'>
                   <div className='swiper-wrapper flex'>
+                    {/* <PosterFilm />
                     <PosterFilm />
                     <PosterFilm />
                     <PosterFilm />
-                    <PosterFilm />
-                    <PosterFilm />
+                    <PosterFilm /> */}
                   </div>
                 </div>
               </div>
@@ -29,6 +31,6 @@ export default function FilmDangChieu() {
           </div>
         </div>
       </section>
-    </div>
+      </div>
   );
 }
